89ed23c0221ca36c16fd189a40f1086260282fc2
John Lagoutaris <jjlagoutaris@gmail.com>
2022-03-13 04:07:37 -0400

    Add the default page styling separate content into header, sidebar, and main section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.scss';
+import logo from './todoList.png';
 import Todo from './todo.js';
 import Project from './project.js';
 
@@ -6,6 +7,3 @@ let todoOne = Todo("Take out trash", "self-explanatory", "11/12/13", "top");
 
 let projOne = Project("Chores", "Responsibilities within and outside the household");
 projOne.addToList(todoOne);
-projOne.arr[0].setTitle("Fuck bitches");
-
-console.log(projOne.arr[0].getTitle());
